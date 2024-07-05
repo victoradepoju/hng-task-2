d84471b1ecc94216c4eabad3d82c21d00c466c68
victoradepoju <victoradepoju30@gmail.com>
2024-07-05 11:31:07 +0100

    setup docker and dependencies

---
+++ src/test/java/com/example/hng_task2/HngTask2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hng_task2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HngTask2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
