9ff80898e77addd964efd51dab3b44fa726f5db2
victoradepoju <victoradepoju30@gmail.com>
2024-07-07 21:54:24 +0100

    fixed validation

--- src/main/java/com/victor/hng_task2/dto/LoginRequest.java
+++ src/main/java/com/victor/hng_task2/dto/LoginRequest.java
@@ -2,12 +2,15 @@ package com.victor.hng_task2.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record LoginRequest(
         @Email(message = "Email is not properly formatted")
+        @NotNull
         @NotBlank(message = "Email is needed for login")
         String email,
 
+        @NotNull
         @NotBlank(message = "Password is needed for login")
         String password
 ) {
