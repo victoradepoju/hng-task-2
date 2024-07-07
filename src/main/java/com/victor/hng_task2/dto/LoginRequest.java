143094f9df61fce2c4c084b3d2f1070a2370da05
victoradepoju <victoradepoju30@gmail.com>
2024-07-07 22:00:16 +0100

    commit

--- src/main/java/com/victor/hng_task2/dto/LoginRequest.java
+++ src/main/java/com/victor/hng_task2/dto/LoginRequest.java
@@ -6,11 +6,9 @@ import jakarta.validation.constraints.NotNull;
 
 public record LoginRequest(
         @Email(message = "Email is not properly formatted")
-        @NotNull
         @NotBlank(message = "Email is needed for login")
         String email,
 
-        @NotNull
         @NotBlank(message = "Password is needed for login")
         String password
 ) {
--- src/main/java/com/victor/hng_task2/dto/RegisterRequest.java
+++ src/main/java/com/victor/hng_task2/dto/RegisterRequest.java
@@ -2,23 +2,18 @@ package com.victor.hng_task2.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record RegisterRequest (
-        @NotNull
         @NotBlank(message = "Firstname is needed for registration")
         String firstName,
 
-        @NotNull
         @NotBlank(message = "Lastname is needed for registration")
         String lastName,
 
-        @NotNull
         @Email(message = "Email is not properly formatted")
         @NotBlank(message = "Email is needed for registration")
         String email,
 
-        @NotNull
         @NotBlank(message = "Password is needed for registration")
         String password,
 
